b953194019a9dfdcaae602f938a04d4b5fefe5a2
ericc <ericc@LAPTOP-97HVV20R>
2023-02-18 14:20:09 +0100

    Add project files.

---
+++ 3sem oblikatorisk opgave 1/Car.cs
@@ -0,0 +1,44 @@
+﻿namespace _3sem_oblikatorisk_opgave_1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+        public Car()
+        {
+
+        }
+        public void ValidateId()
+        {
+            if (Id < 0)
+            {
+                throw new ArgumentOutOfRangeException(" Faulty Id: Id doesn't exist");
+            }
+        }
+        public void ValidateModel()
+        {
+            if (Model.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException($"The string {Model} must be at least 4 chars long. Its current length is {Model.Length}");
+
+            }
+        }
+        public void ValidatePrice()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException($"Error price cannot be negative");
+
+            }
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate.Length <= 2 & LicensePlate.Length <= 7)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ 3sem oblikatorisk opgave 1Tests/CarTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3sem_oblikatorisk_opgave_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3sem_oblikatorisk_opgave_1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car cartest = new Car()
+        {
+            Id = -1,
+            LicensePlate = "as",
+            Price = -1,
+            Model = "bmw",
+
+
+        };
+
+        [TestMethod()]
+        public void ValidateIdTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => cartest.ValidateId());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => cartest.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => cartest.ValidateLicensePlate());
+        }
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => cartest.ValidateModel());
+        }
+    }
+}
